2a347314368713573aa6a9785089bd7cc8009dc6
Laurent Bercot <ska-skaware@skarnet.org>
2016-04-27 16:50:32 +0000

    Use skalibs touch() in s6-touch for more portability

--- src/skaembutils/s6-touch.c
+++ src/skaembutils/s6-touch.c
@@ -1,8 +1,5 @@
 /* ISC license. */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <time.h>
 #include <skalibs/strerr2.h>
 #include <skalibs/djbunix.h>
 
@@ -10,16 +7,9 @@
 
 int main (int argc, char const *const *argv)
 {
-  static struct timespec const now2[2] = { { .tv_sec = 0, .tv_nsec = UTIME_NOW }, { .tv_sec = 0, .tv_nsec = UTIME_NOW } } ;
   char const *const *p = argv + 1 ;
   PROG = "s6-touch" ;
   if (argc < 2) strerr_dieusage(100, USAGE) ;
-  for (; *p ; p++)
-  {
-    register int fd = open_append(*p) ;
-    if (fd < 0) strerr_diefu2sys(111, "open_append ", *p) ;
-    if (futimens(fd, now2) < 0) strerr_diefu2sys(111, "futimens ", *p) ; 
-    fd_close(fd) ;
-  }
+  for (; *p ; p++) if (!touch(*p)) strerr_diefu2sys(111, "touch ", *p) ;
   return 0 ;
 }
